4b5284b0889ba241f5f29176f3890e4d1d1b47be
Gareth Bowles <gareth@optimalops.net>
2015-04-29 15:45:04 -0700

    Fixed PEP8 warnings

--- MakeDiamond.py
+++ MakeDiamond.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import string   
+import string
 
 __version__ = '0.0.1'
 
@@ -9,8 +9,10 @@ Given values w and o, print a diamond of asterisks with width w,
 indented by o spaces.
 
 w must be an odd number.
-""" 
-class MakeDiamond(object): 
+"""
+
+
+class MakeDiamond(object):
 
     def __init__(self, width, offset):
         self.width = width
@@ -24,7 +26,7 @@ class MakeDiamond(object):
         # Validate parameters
         if (width % 2 == 0 or width <= 0):
             print "Width must be a positive odd number."
-            return 
+            return
         if offset < 0:
             print "Offset must be zero or more."
             return
@@ -45,5 +47,5 @@ class MakeDiamond(object):
             print self.diamond[i]
 
 if __name__ == "__main__":
-    diamond = MakeDiamond(0,-4)
+    diamond = MakeDiamond(0, -4)
     diamond.printit()
--- TestMakeDiamond.py
+++ TestMakeDiamond.py
@@ -4,38 +4,38 @@
 import unittest
 from MakeDiamond import MakeDiamond
 
+
 class TestMakeDiamond(unittest.TestCase):
     def test_width_3_offset_0(self):
         expected = [" * ", "***", " * "]
-        diamond = MakeDiamond(3,0)
+        diamond = MakeDiamond(3, 0)
         actual = diamond.construct()
         print "Testing with width 3, offset 0 - should print a diamond"
         diamond.printit()
-        print "" 
+        print ""
         self.assertTrue(actual == expected)
 
     def test_width_7_offset_4(self):
         expected = ["       *   ", "      ***  ", "     ***** ", "    *******",
                     "     ***** ", "      ***  ", "       *   "]
-        diamond = MakeDiamond(7,4)
+        diamond = MakeDiamond(7, 4)
         actual = diamond.construct()
         print "Testing with width 7, offset 4 - should print a diamond"
-        diamond.printit() 
+        diamond.printit()
         print ""
         self.assertTrue(actual == expected)
 
     def test_invalid_width(self):
-        diamond = MakeDiamond(0,4)
+        diamond = MakeDiamond(0, 4)
         print "Testing with width 0 - should print an error"
         actual = diamond.construct()
-        self.assertTrue(actual == None)
-
+        self.assertTrue(actual is None)
 
     def test_invalid_offset(self):
         print "Testing with width 7, offset -4 - should print an error"
-        diamond = MakeDiamond(7,-4)
+        diamond = MakeDiamond(7, -4)
         actual = diamond.construct()
-        self.assertTrue(actual == None)
+        self.assertTrue(actual is None)
 
 if __name__ == '__main__':
     unittest.main()
